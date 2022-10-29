e221a2af5d19fcdbb42b6849178b735b9fe4cb27
code-with-onye <onyemachitrust@gmail.com>
2022-10-28 23:17:10 -0700

    update

--- src/components/TermsModal.tsx
+++ src/components/TermsModal.tsx
@@ -8,6 +8,57 @@ export interface TermsModalProps {
   onCloseTermsModal: () => void;
 }
 
+const TermsData = [
+  {
+    heading: "  TERMS OF SERVICE",
+    desc: ` NaijaPlayStore is a Nigerian music streaming service, registered under
+          the corporate affairs commission (CAC) of Nigeria. Before using the
+          services, carefully read the terms. You accept the terms by using the
+          services in any manner. If you do not agree to the Terms, do not use
+          the services.`,
+  },
+  {
+    heading: " Do's and Don'ts",
+    desc: `   Only NFT music or audio can be sold on NaijaPlayStore, any user caught
+          minting (uploading) or auctioning NFT music or audio that belongs to
+          an artist, music producer or creator will be sued for "breech of
+          intellectual property."`,
+  },
+  {
+    heading: " Protection of Data",
+    desc: `    We are concerned about the security of your information and take
+          drastic, appropriate managerial, practical, and concrete precautions
+          to protect and secure all information collected through our website.
+          The security of our systems cannot, however, be completely guaranteed 
+          because no security operation is inaccessible. If any information
+          under our control is compromised due to a security breach, we shall
+          act to take necessary steps to inquire into the event, inform any
+          persons whose information may have been compromised as needed, and
+          take additional action in accordance with any applicable laws and
+          regulations.`,
+  },
+  {
+    heading: "Third-Parties",
+    desc: `For carrying out Crypto currency transactions, we employ
+          the third-party electronic wallet extension MetaMask, Walletconnect,
+          walletlink; your interactions with these extensions are controlled by
+          the applicable privacy regulations.`,
+  },
+  {
+    heading: "Data Access",
+    desc: "On the account settings page, you may view your personal information.",
+  },
+  {
+    heading: "Modify Data",
+    desc: "Your account settings page has a section where you can edit your personal information.",
+  },
+  {
+    heading: "Age Limit",
+    desc: `Collection of personal data from visitors who are younger than 13 is highly prohibited.
+If it comes to our notice that the data of a child under 13, is on our website`,
+  },
+];
+
 const TermsModal: FC<TermsModalProps> = ({ show, onCloseTermsModal }) => {
   const handleClickSubmitForm = () => {
     console.log({ 1: "1" });
@@ -15,27 +66,16 @@ const TermsModal: FC<TermsModalProps> = ({ show, onCloseTermsModal }) => {
 
   const renderContent = () => {
     return (
-      <form action="#">
-        <h3 className="text-lg font-semibold text-neutral-900 dark:text-neutral-200">
-          TERMS OF SERVICE
-        </h3>
-        <span className="text-sm">
-          NaijaPlayStore is a Nigerian music streaming service, registered under
-          the corporate affairs commission (CAC) of Nigeria. Before using the
-          services, carefully read the terms. You accept the terms by using the
-          services in any manner. If you do not agree to the Terms, do not use
-          the services.
-        </span>
-        <br />
-        <h3 className="text-lg font-semibold text-neutral-900 dark:text-neutral-200">
-          Do's and Don'ts
-        </h3>
-        <span>
-          Only NFT music or audio can be sold on NaijaPlayStore, any user caught
-          minting (uploading) or auctioning NFT music or audio that belongs to
-          an artist, music producer or creator will be sued for "breech of
-          intellectual property."
-        </span>
+      <div className="h-72 overflow-y-scroll">
+        {TermsData.map((data, i) => (
+          <>
+            <h3 className="text-lg font-semibold text-neutral-900 dark:text-neutral-200">
+              {data.heading}
+            </h3>
+            <span className="text-sm">{data.desc}</span>
+          </>
+        ))}
+
         <div className="mt-4 space-x-3">
           {/* <ButtonPrimary onClick={handleClickSubmitForm} type="submit">
             Delete
@@ -44,7 +84,7 @@ const TermsModal: FC<TermsModalProps> = ({ show, onCloseTermsModal }) => {
             Cancel
           </ButtonSecondary>
         </div>
-      </form>
+      </div>
     );
   };
 
